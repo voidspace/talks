ab7dac8cf50fa023bb6b6523735564210db46413
Michael Foord <michael@python.org>
2024-05-01 23:31:13 +0100

    Update

--- pytest/test_first.py
+++ pytest/test_first.py
@@ -7,3 +7,29 @@ def test_function():
 def test_failing_test():
     result = 1 + 2
     assert result == 4
+
+
+def test_not_equals():
+    bad_result = 'not this'
+    assert 'actual result' != bad_result
+
+def test_comparisons():
+    result = 6
+    assert result > 5
+    assert result < 9
+    assert result >= 6
+    assert result <= 6
+
+
+def test_is_not_operator():
+    expected = object()
+    actual = object()
+
+    assert actual is not expected
+
+
+def test_is_operator():
+    expected = object()
+    actual = expected
+
+    assert actual is expected
